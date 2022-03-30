a831650b4478ca33aab397cd5eb2d5dcb783ae56
xconnortv <connorbreuer@gmail.com>
2022-03-30 12:25:28 +0200

    Krass²

--- src/components/App.js
+++ src/components/App.js
@@ -5,18 +5,21 @@ import UserSelection from "../pages/UserSelection";
 import Index from "../pages/Index";
 import NewUser from "../pages/NewUser";
 import User from "../pages/User";
-
+import { CookiesProvider } from "react-cookie";
 function App() {
   return (
-    <div className="app">
-      <Routes>
-        <Route path="/" element={<Index />} />
-          <Route exact path="/users" element={<UserSelection />} />
-          <Route exact path="/new" element={<NewUser />} />
-          <Route exact path="/user/:uuid" element={<User />} />
-          <Route path="*" element={<Navigate to="/"/>} />
-      </Routes>
-    </div>
+      <CookiesProvider>
+          <div className="app">
+              <Routes>
+                  <Route path="/" element={<Index />} />
+                  <Route exact path="/users" element={<UserSelection />} />
+                  <Route exact path="/new" element={<NewUser />} />
+                  <Route exact path="/user/:uuid" element={<User />} />
+                  <Route path="*" element={<Navigate to="/"/>} />
+              </Routes>
+          </div>
+      </CookiesProvider>
+
   );
 }
 
--- src/pages/UserSelection.js
+++ src/pages/UserSelection.js
@@ -2,15 +2,22 @@ import {useEffect, useState} from "react";
 import './styles/UserSelections.css';
 import axios from "axios"
 import UserComponent from "../components/UserComponent";
+import {useCookies} from "react-cookie";
 function UserSelection() {
 
     const [users, setUsers] = useState([])
-
+    const [cookies, setCookie, removeCookie] = useCookies(["user"])
     useEffect(() => {
+
+        if(cookies.uuid){
+            removeCookie("uuid", {path:"/"})
+            console.log(cookies)
+        }
+
         axios.post("http://localhost:3000/api/user/get").then(res => {
             setUsers(res.data)
         })
-    },[])
+    },[cookies.uuid, removeCookie])
     return (
         <div className="selection">
              <div className="users">
